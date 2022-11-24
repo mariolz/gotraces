58b1a6362d0a74a334c9acdc396aa2270df80e8e
mariolz <997388772@qq.com>
2022-11-24 09:13:25 +0000

    update gotrace

--- trace.go
+++ trace.go
@@ -1,45 +1,55 @@
 package gotraces
+
+import (
+	"bytes"
+	"fmt"
+	"runtime"
+	"strconv"
+	"sync"
+)
+
 func getGID() uint64 {
-    b := make([]byte, 64)
-    b = b[:runtime.Stack(b, false)]
-    b = bytes.TrimPrefix(b, []byte("goroutine "))
-    b = b[:bytes.IndexByte(b, ' ')]
-    n, _ := strconv.ParseUint(string(b), 10, 64)
-    return n
+	b := make([]byte, 64)
+	b = b[:runtime.Stack(b, false)]
+	b = bytes.TrimPrefix(b, []byte("goroutine "))
+	b = b[:bytes.IndexByte(b, ' ')]
+	n, _ := strconv.ParseUint(string(b), 10, 64)
+	return n
 }
+
 // github.com/bigwhite/experiments/trace-function-call-chain/trace4/trace.go
 
 var mu sync.Mutex
 var m = make(map[uint64]int)
 
 func printTrace(id uint64, name, typ string, indent int) {
-    indents := ""
-    for i := 0; i < indent; i++ {
-        indents += "\t"
-    }
-    fmt.Printf("g[%02d]:%s%s%s\n", id, indents, typ, name)
+	indents := ""
+	for i := 0; i < indent; i++ {
+		indents += "\t"
+	}
+	fmt.Printf("g[%02d]:%s%s%s\n", id, indents, typ, name)
 }
 
 func trace() func() {
-    pc, _, _, ok := runtime.Caller(1)
-    if !ok {
-        panic("not found caller")
-    }
+	pc, _, _, ok := runtime.Caller(1)
+	if !ok {
+		panic("not found caller")
+	}
 
-    id := getGID()
-    fn := runtime.FuncForPC(pc)
-    name := fn.Name()
+	id := getGID()
+	fn := runtime.FuncForPC(pc)
+	name := fn.Name()
 
-    mu.Lock()
-    v := m[id]
-    m[id] = v + 1
-    mu.Unlock()
-    printTrace(id, name, "->", v+1)
-    return func() {
-        mu.Lock()
-        v := m[id]
-        m[id] = v - 1
-        mu.Unlock()
-        printTrace(id, name, "<-", v)
-    }
+	mu.Lock()
+	v := m[id]
+	m[id] = v + 1
+	mu.Unlock()
+	printTrace(id, name, "->", v+1)
+	return func() {
+		mu.Lock()
+		v := m[id]
+		m[id] = v - 1
+		mu.Unlock()
+		printTrace(id, name, "<-", v)
+	}
 }
